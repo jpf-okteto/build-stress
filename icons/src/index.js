7f58f7ced1b5297f9ca9654e07f5674b87c279a6
jpf-okteto <92427625+jpf-okteto@users.noreply.github.com>
2025-10-29 16:34:36 +0100

    Add icon imports for various libraries

---
+++ icons/src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'simple-icons/icons/react';
+
+console.log('Web stress test build with many small files');
